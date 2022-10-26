eca986bb02a8d300d9ecbf7c785091725687656a
Felix Hartung <f.hartung@posteo.de>
2022-10-26 19:38:57 +0200

    data-classes for buttons added

---
+++ script.js
@@ -0,0 +1,7 @@
+const numberButtons = document.querySelectorAll("[data-number]");
+const operationButtons = document.querySelectorAll("[data-operation]");
+const equalsButton = document.querySelector("[data-equals]");
+const clearButton = document.querySelector("[data-clear]");
+const backButton = document.querySelector("[data-back]");
+const upperScreenText = document.querySelector("[data-upper-screen]");
+const lowerScreenText = document.querySelector("[data-lower-screen]");
