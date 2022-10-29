fba54eccd8b90a8b254830cc605c549bd3278dad
Felix Hartung <f.hartung@posteo.de>
2022-10-29 18:17:24 +0200

    renamed some buttons, added calculator class

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+class Claculator {
+  constructor(previousOperandText, currentOperandText) {
+    this.previousOperandText = previousOperandText;
+    this.currentOperandText = currentOperandText;
+  }
+
+  clear() {
+
+  }
+
+  delete() {
+    
+  }
+
+}
+
 const numberButtons = document.querySelectorAll("[data-number]");
 const operationButtons = document.querySelectorAll("[data-operation]");
 const equalsButton = document.querySelector("[data-equals]");
 const clearButton = document.querySelector("[data-clear]");
 const backButton = document.querySelector("[data-back]");
-const upperScreenText = document.querySelector("[data-upper-screen]");
-const lowerScreenText = document.querySelector("[data-lower-screen]");
+const previousOperandText = document.querySelector("[data-previous-operand]");
+const currentOperandText = document.querySelector("[data-current-operand]");
